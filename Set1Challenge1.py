07094fb3930747872c36f6d011802f5ca0081d17
Stephanie Oh <oh.stephanie.s@gmail.com>
2015-01-27 22:46:51 -0800

    Deleted raw_input

--- Set1Challenge1.py
+++ Set1Challenge1.py
@@ -1,12 +1,12 @@
 import binascii
 
+def hex2base64(x):
+	return x.decode('hex').encode('base64')
+
+
+
 a = binascii.unhexlify("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 
 binascii.b2a_base64(a)
 #things get a bit weird since len("49276d...") > 57.
 
-def hex2base64(input):
-	return input.decode('hex').encode('base64')
-
-input = raw_input()
-print "======================================================= \n", hex2base64(input)
