cebf6b6fd0b8ccf04a6a2301f384cd39e2b7bf05
Stephanie Oh <oh.stephanie.s@gmail.com>
2015-01-27 22:10:43 -0800

    Defined a function for future use

--- Set1Challenge1.py
+++ Set1Challenge1.py
@@ -5,6 +5,8 @@ a = binascii.unhexlify("49276d206b696c6c696e6720796f757220627261696e206c696b6520
 binascii.b2a_base64(a)
 #things get a bit weird since len("49276d...") > 57.
 
+def hex2base64(input):
+	return input.decode('hex').encode('base64')
+
 input = raw_input()
-ans = input.decode('hex').encode('base64')
-print "======================================================= \n", ans
+print "======================================================= \n", hex2base64(input)
